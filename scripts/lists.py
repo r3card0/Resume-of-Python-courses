676c605a1a06d33ed785c649a64705fc3fd3ee91
r3card0 <ricardo.rdz@pm.me>
2023-01-18 17:05:32 -0600

    added pop and remove methods

--- scripts/lists.py
+++ scripts/lists.py
@@ -15,8 +15,18 @@ def addElement(list:list,string:str):
     return list
 
 # function to add an element in a list : insert
-def insertElement(list:list,string:str,pos:int):
+def insertElement(list:list,string,pos:int):
     list.insert(pos,string)
+    return list
+
+# function to remove an element: pop
+def removeElement(list:list,pos:int):
+    list.pop(pos)
+    return list
+
+# function to remove an element: remove
+def removeElementRemove(list:list,string):
+    list.remove(string)
     return list
 
 def run():
@@ -24,6 +34,9 @@ def run():
     print(addElement(tectonic_plates, 'North-American plate'))
     print(addElement(tectonic_plates, 'Eurasian plate'))
     print(insertElement(tectonic_plates,'Philipine plate',1))
+    print(removeElement(tectonic_plates,2))
+    print(insertElement(tectonic_plates,['Nazca plate','South-American plate'],0))
+    print(removeElementRemove(tectonic_plates,'North-American plate'))
 
 
 if __name__ == "__main__":
